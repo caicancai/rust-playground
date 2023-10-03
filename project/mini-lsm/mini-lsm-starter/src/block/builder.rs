912b0d350dff692654b2906f814567b07fc0924a
caicancai <2356672992@qq.com>
2023-10-03 11:57:57 +0800

    mini-lsm day1

--- project/mini-lsm/mini-lsm-starter/src/block/builder.rs
+++ project/mini-lsm/mini-lsm-starter/src/block/builder.rs
@@ -4,27 +4,53 @@
 use super::Block;
 
 /// Builds a block.
-pub struct BlockBuilder {}
+pub struct BlockBuilder {
+    offsets: Vec<u16>,
+
+    data: Vec<u8>,
+
+    block_size: usize,
+}
 
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets:    Vec::new(),
+            data:       Vec::new(),
+            block_size,
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: &[u8], value: &[u8]) -> bool {
-        unimplemented!()
+        assert!(!key.is_empty(), "key must not be empty");
+
+        if self.estimated_size() + key_len() + value.len() + SIZEOF_u16 * 3 > self.block_size && !self.is_empty(){
+            return false;
+        }
+        self.offsets.push(self.data.len() as u16);
+        self.data.put_u16(key.len() as u16);
+        self.data.put(key);
+        self.data.put_u16(value.len() as u16);
+        self.data.put(value);
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.offset.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        if self.is_empty(){
+            panic!("block should not be empty");
+        }
+        Block {
+            data:    self.data,
+            offsets: self.offsets,
+        }
     }
 }
