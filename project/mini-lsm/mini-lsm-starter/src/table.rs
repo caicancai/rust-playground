b0d69015e1805d8a3c751b917230737ccdca8cbe
caicancai <2356672992@qq.com>
2023-11-04 22:10:13 +0800

    mem_table

--- project/mini-lsm/mini-lsm-starter/src/table.rs
+++ project/mini-lsm/mini-lsm-starter/src/table.rs
@@ -1,15 +1,13 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 mod builder;
 mod iterator;
 
+use std::fs::File;
 use std::path::Path;
 use std::sync::Arc;
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 pub use builder::SsTableBuilder;
-use bytes::{Buf, Bytes};
+use bytes::{Buf, BufMut, Bytes};
 pub use iterator::SsTableIterator;
 
 use crate::block::Block;
@@ -19,84 +17,104 @@ use crate::lsm_storage::BlockCache;
 pub struct BlockMeta {
     /// Offset of this data block.
     pub offset: usize,
-    /// The first key of the data block, mainly used for index purpose.
+    /// The first key of the data block.
     pub first_key: Bytes,
 }
 
 impl BlockMeta {
     /// Encode block meta to a buffer.
-    /// You may add extra fields to the buffer,
-    /// in order to help keep track of `first_key` when decoding from the same buffer in the future.
-    pub fn encode_block_meta(block_meta: &[BlockMeta],buf: &mut Vec<u8>) {
+    pub fn encode_block_meta(block_meta: &[BlockMeta], buf: &mut Vec<u8>) {
         let mut estimated_size = 0;
         for meta in block_meta {
+            // The size of offset
             estimated_size += std::mem::size_of::<u32>();
-
+            // The size of key length
             estimated_size += std::mem::size_of::<u16>();
-
+            // The size of actual key
             estimated_size += meta.first_key.len();
         }
+        // Reserve the space to improve performance, especially when the size of incoming data is large
         buf.reserve(estimated_size);
         let original_len = buf.len();
         for meta in block_meta {
             buf.put_u32(meta.offset as u32);
-            buf.put_u16(meta.first_key_len() as u16);
+            buf.put_u16(meta.first_key.len() as u16);
             buf.put_slice(&meta.first_key);
         }
         assert_eq!(estimated_size, buf.len() - original_len);
     }
 
     /// Decode block meta from a buffer.
-    pub fn decode_block_meta(buf: impl Buf) -> Vec<BlockMeta> {
+    pub fn decode_block_meta(mut buf: impl Buf) -> Vec<BlockMeta> {
         let mut block_meta = Vec::new();
         while buf.has_remaining() {
-            let offset          = buf.get_u32() as usize;
-            let first_key_len   = buf.get_u16() as usize;
-            let first_key       = buf.copy_to_bytes(first_key_len);
-            block_meta.push(BlockMeta {offset, first_key});
+            let offset = buf.get_u32() as usize;
+            let first_key_len = buf.get_u16() as usize;
+            let first_key = buf.copy_to_bytes(first_key_len);
+            block_meta.push(BlockMeta { offset, first_key });
         }
         block_meta
     }
 }
 
 /// A file object.
-pub struct FileObject(Bytes);
+///
+/// Before day 4, it should look like:
+///
+/// ```ignore
+/// pub struct FileObject(Bytes);
+///
+/// impl FileObject {
+///     pub fn read(&self, offset: u64, len: u64) -> Result<Vec<u8>> {
+///         Ok(self.0[offset as usize..(offset + len) as usize].to_vec())
+///    }
+///     pub fn size(&self) -> u64 {
+///         self.0.len() as u64
+///     }
+///
+///     pub fn create(_path: &Path, data: Vec<u8>) -> Result<Self> {
+///         Ok(FileObject(data.into()))
+///     }
+///
+///     pub fn open(_path: &Path) -> Result<Self> {
+///         unimplemented!()
+///     }
+/// }
+/// ```
+pub struct FileObject(File, u64);
 
 impl FileObject {
     pub fn read(&self, offset: u64, len: u64) -> Result<Vec<u8>> {
-        Ok(self.0[offset as usize..(offset + len) as usize].to_vec())
+        use std::os::unix::fs::FileExt;
+        let mut data = vec![0; len as usize];
+        self.0.read_exact_at(&mut data[..], offset)?;
+        Ok(data)
     }
 
     pub fn size(&self) -> u64 {
-        self.0.len() as u64
+        self.1
     }
 
     /// Create a new file object (day 2) and write the file to the disk (day 4).
     pub fn create(path: &Path, data: Vec<u8>) -> Result<Self> {
-        std::fs::write(path, &data);
+        std::fs::write(path, &data)?;
         Ok(FileObject(
             File::options().read(true).write(false).open(path)?,
             data.len() as u64,
         ))
     }
 
-    pub fn open(path: &Path) -> Result<Self> {
+    pub fn open(_path: &Path) -> Result<Self> {
         unimplemented!()
     }
 }
 
-/// -------------------------------------------------------------------------------------------------------
-/// |              Data Block             |             Meta Block              |          Extra          |
-/// -------------------------------------------------------------------------------------------------------
-/// | Data Block #1 | ... | Data Block #N | Meta Block #1 | ... | Meta Block #N | Meta Block Offset (u32) |
-/// -------------------------------------------------------------------------------------------------------
 pub struct SsTable {
-    /// The actual storage unit of SsTable, the format is as above.
     file: FileObject,
-    /// The meta blocks that hold info for data blocks.
     block_metas: Vec<BlockMeta>,
-    /// The offset that indicates the start point of meta blocks in `file`.
     block_meta_offset: usize,
+    id: usize,
+    block_cache: Option<Arc<BlockCache>>,
 }
 
 impl SsTable {
@@ -110,11 +128,11 @@ impl SsTable {
         let len = file.size();
         let raw_meta_offset = file.read(len - 4, 4)?;
         let block_meta_offset = (&raw_meta_offset[..]).get_u32() as u64;
-        let raw_meta = file.add(block_meta_offset, len - 4 - block_meta_offset)?;
+        let raw_meta = file.read(block_meta_offset, len - 4 - block_meta_offset)?;
         Ok(Self {
             file,
             block_metas: BlockMeta::decode_block_meta(&raw_meta[..]),
-            block_meta_offset: block_meta_offset as uszie;
+            block_meta_offset: block_meta_offset as usize,
             id,
             block_cache,
         })
@@ -123,28 +141,38 @@ impl SsTable {
     /// Read a block from the disk.
     pub fn read_block(&self, block_idx: usize) -> Result<Arc<Block>> {
         let offset = self.block_metas[block_idx].offset;
-        let offset_end = self.block_metas.get(block_idx + 1).map_or(self.block_meta_offset, |x| x.offset);
+        let offset_end = self
+            .block_metas
+            .get(block_idx + 1)
+            .map_or(self.block_meta_offset, |x| x.offset);
         let block_data = self
-        .file
-        .read(offset as u64, (offset_end - offset) as u64)?;
+            .file
+            .read(offset as u64, (offset_end - offset) as u64)?;
         Ok(Arc::new(Block::decode(&block_data[..])))
     }
 
-    /// Read a block from disk, with block cache. (Day 4)
+    /// Read a block from disk, with block cache.
     pub fn read_block_cached(&self, block_idx: usize) -> Result<Arc<Block>> {
-        unimplemented!()
+        if let Some(ref block_cache) = self.block_cache {
+            let blk = block_cache
+                .try_get_with((self.id, block_idx), || self.read_block(block_idx))
+                .map_err(|e| anyhow!("{}", e))?;
+            Ok(blk)
+        } else {
+            self.read_block(block_idx)
+        }
     }
 
     /// Find the block that may contain `key`.
-    /// Note: You may want to make use of the `first_key` stored in `BlockMeta`.
-    /// You may also assume the key-value pairs stored in each consecutive block are sorted.
     pub fn find_block_idx(&self, key: &[u8]) -> usize {
-        unimplemented!()
+        self.block_metas
+            .partition_point(|meta| meta.first_key <= key)
+            .saturating_sub(1)
     }
 
     /// Get number of data blocks.
     pub fn num_of_blocks(&self) -> usize {
-        unimplemented!()
+        self.block_metas.len()
     }
 }
 
